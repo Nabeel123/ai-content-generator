6707881d6f0da4b6e154bf14c77126027cb5dc1a
Nabeel Francis <levisdevis66@gmail.com>
2024-11-20 21:21:51 +0100

    Updated code with template card.

--- app/(data)/Templates.tsx
+++ app/(data)/Templates.tsx
@@ -3,7 +3,7 @@ export default [
     name: "Blog Title",
     desc: "An AI tool that generate blog title depends on yout blog information",
     category: "Blog",
-    icon: "./blog.png",
+    icon: "https://cdn-icons-png.flaticon.com/128/4905/4905454.png",
     aiPrompt:
       "Give me 5 blog topic idea in bullet wise only based on give niche & outline and give me result in Rich text editor format",
     slug: "generate-blog-title",
--- app/dashboard/_component/TemplateListSection.tsx
+++ app/dashboard/_component/TemplateListSection.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Templates from "@/(data)/Templates";
+import TemplateCard from "./TemplateCard";
+
+export interface FORM {
+  label: string;
+  field: string;
+  name: string;
+  required?: boolean;
+}
+export interface TEMPLATE {
+  name: string;
+  desc: string;
+  icon: string;
+  category: string;
+  slug: string;
+  aiPrompt: string;
+  form?: FORM[];
+}
 
 function TemplateListSection() {
-  return <div></div>;
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 p-10">
+      {Templates.map((item: TEMPLATE, index: number) => (
+        <div key={index}>
+          <TemplateCard {...item} />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default TemplateListSection;
